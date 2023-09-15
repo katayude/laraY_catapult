58a401a4b000405185493f709bac5474f55669ce
katayude <xXgucchiXx@gmail.com>
2023-09-15 17:41:48 +0900

    叩いた！

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -28,6 +28,14 @@
                         {{ __('Create') }}
                     </x-nav-link>
                 </div>
+                <!-- 🔽 マイページへのリンクを追加 -->
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('tweet.mypage')" :active="request()->routeIs('tweet.mypage')">
+                        {{ __('Mypage') }}
+                    </x-nav-link>
+                </div>
+            </div>
+
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
@@ -92,6 +100,12 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('tweet.create')" :active="request()->routeIs('tweet.create')">
                 {{ __('Create') }}
+            </x-responsive-nav-link>
+        </div>
+        <!-- 🔽 マイページへのリンクを追加 -->
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('tweet.mypage')" :active="request()->routeIs('tweet.mypage')">
+                {{ __('Mypage') }}
             </x-responsive-nav-link>
         </div>
 
